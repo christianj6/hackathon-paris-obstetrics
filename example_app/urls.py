4fa3e017c5ceaaa9b30ebb5dc493157a2cf0c787
luananama <luana.mariav@gmail.com>
2020-02-23 18:17:23 +0100

    small changes to buddy page appearance

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name='example-home'),
     path('about/', views.about, name='example-about')
     # path('about/', views.about, name='example-about')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
